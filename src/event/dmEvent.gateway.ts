6f2622453b2e0700af1300da8f458fe7169be0eb
parksy-13 <psy9905@gmail.com>
2024-04-13 10:36:04 +0900

    임시 저장

--- src/event/dmEvent.gateway.ts
+++ src/event/dmEvent.gateway.ts
@@ -10,15 +10,30 @@ import {
 } from '@nestjs/websockets';
 
 import { Server, Socket } from 'socket.io';
+import { DMService } from 'src/dm/dm.service';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway(81, { namespace: 'direct_message' })
 export class DMGateway {
+  constructor(private readonly dmService: DMService) {}
   @WebSocketServer()
-  server;
+  server: Server;
+  private logger: Logger = new Logger('DMsGateway');
 
   wsClients = [];
 
   @SubscribeMessage('joinDM')
-  handleJoinDM(@MessageBody() data: {}, @ConnectedSocket() client: Socket) {}
+  handleJoinDM(@MessageBody() data: { dmRoomId: number }, @ConnectedSocket() client: Socket) {
+    const { dmRoomId } = data;
+    const dmRoomName = `DMRoom: ${dmRoomId}`;
+    client.join(dmRoomName);
+    console.log('join', '참여 완료');
+  }
+
+  @SubscribeMessage('sendMessage')
+  async handleMessage(@MessageBody() message: { dmRoomId: number }, @ConnectedSocket() client: Socket) {
+    const { dmRoomId } = message;
+    const dmRoomName = `DMRoom: ${dmRoomId}`;
+  }
+  // TODO: mongoDB 연결하기
 }
--- src/event/dmEvent.module.ts
+++ src/event/dmEvent.module.ts
@@ -1,2 +1,16 @@
 import { Module } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
+import { DMGateway } from './dmEvent.gateway';
+import { Friendship } from 'src/friend/entities/friendship.entity';
+import { FriendDMs } from 'src/dm/entities/friendDMs.entity';
+import { DMRoom } from 'src/dm/entities/DM-room.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DMModule } from 'src/dm/dm.module';
+import { DMService } from 'src/dm/dm.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Friendship, FriendDMs, DMRoom]), DMModule],
+  providers: [DMGateway, DMService],
+  exports: [DMGateway],
+})
+export class DmEventModule {}
