01d26307cf99e79285438fed2fefa7cddde7ae63
parksy-13 <psy9905@gmail.com>
2024-04-29 01:54:27 +0900

    :bug: Fix : 빠진 회원가입 함수 다시 추가

--- public/mainPage.js
+++ public/mainPage.js
@@ -19,7 +19,7 @@ window.onload = function () {
 };
 
 function checkLoginStatus(){
-  fetch('http://chuncik.store:3000/user/checkLogin',{
+  fetch('http://localhost:3000/user/checkLogin',{
         method:'GET',
       })
     .then(res => {
@@ -53,36 +53,33 @@ function showLoggedOutUI() {
   document.getElementById('login').style.display = 'block';
 }
 
+function goSignup(event) {
+  console.log('회원 가입');
+  window.location.href = 'http://localhost:3000/user/sign-up';
+}
+
 async function goLogin(event) {
   console.log('로그인');
-  window.location.href = 'http://chuncik.store:3000/user/login';
+  window.location.href = 'http://localhost:3000/user/login';
   }
 
-function showLoggedOutUI() {
-  document.getElementById('logout').style.display = 'none';
-  document.getElementById('profile').style.display = 'none';
-  document.getElementById('dm').style.display = 'none';
-  document.getElementById('sign-up').style.display = 'block';
-  document.getElementById('login').style.display = 'block';
-}
-
-function goChannel(event) {
-  console.log('채널 입장');
-  window.location.href = 'chat';
-}
+  function goChannel(event) {
+    console.log('채널 입장');
+    window.location.href = 'http://localhost:3000/channel/list';
+  }
 
 function goDM(event) {  
   console.log('dm 입장');
-  window.location.href = 'http://chuncik.store:3000/dm';
+  window.location.href = 'http://localhost:3000/dm';
 }
 
 function goProgile(event){
     console.log('프로필 이동');
-    window.location.href = 'http://chuncik.store:3000/user/userinfo'
+    window.location.href = 'http://localhost:3000/user/userinfo'
   }
 
 
 function goPost(event) {
   console.log('커뮤니티 이동');
-  window.location.href = 'http://chuncik.store/post/page';
+  window.location.href = 'http://localhost:3000/post/page';
 }
