e5bef248a27262e404f1bc645af209387167a1bb
TriumphOne <woo001232naver.com>
2024-04-30 01:54:38 +0900

    :recycle: Refactor: chuncik에서 chunsik으로 변경

--- public/login.js
+++ public/login.js
@@ -21,7 +21,7 @@ function login() {
       const token = json.accessToken;
       window.localStorage.setItem('authorization', token);
 
-      window.location.href = 'http://chuncik.store:3000/main';
+      window.location.href = 'http://chunsik.store:3000/main';
     })
     .catch(error => console.error('Error:', error));
 }
