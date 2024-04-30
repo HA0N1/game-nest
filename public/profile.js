2bdecff1fd5bf0262d0d9a8a6c19c7978a8ea684
HA0N1 <intor48@gmail.com>
2024-04-30 16:21:44 +0900

    test

--- public/profile.js
+++ public/profile.js
@@ -1,40 +1,40 @@
 const token = window.localStorage.getItem('authorization');
 const profileImage = document.getElementById('profileImage');
-const nickname = document.getElementById('nickname')
-const email = document.getElementById('email')
-const userId = document.getElementById('userId')
+const nickname = document.getElementById('nickname');
+const email = document.getElementById('email');
+const userId = document.getElementById('userId');
 const interestGenres = document.getElementById('interestGenres');
 const dmBtn = document.getElementById('dmBtn');
 const findFriends = document.getElementById('findFriends');
 const registerFriends = document.getElementById('registerFriend');
 
-window.onload = function (){
-    checkLoginStatus();
-    UserProfiles();
-}
+window.onload = function () {
+  checkLoginStatus();
+  UserProfiles();
+};
 
-dmBtn.addEventListener('click', function(){
-    window.location.href = 'https://chunsik.store:3000/dm'
-})
+dmBtn.addEventListener('click', function () {
+  window.location.href = 'https://chunsik.store/dm';
+});
 
-findFriends.addEventListener('click', function(){
-    window.location.href = 'https://chunsik.store:3000/user/findFriends'
-})
+findFriends.addEventListener('click', function () {
+  window.location.href = 'https://chunsik.store/user/findFriends';
+});
 
-registerFriends.addEventListener('click', function(){
-    window.location.href = 'https://chunsik.store:3000/user/getFriends'
-})
+registerFriends.addEventListener('click', function () {
+  window.location.href = 'https://chunsik.store/user/getFriends';
+});
 
-function checkLoginStatus(){
-    fetch('https://chunsik.store:3000/user/checkLogin',{
-        method:'GET',
-      })
+function checkLoginStatus() {
+  fetch('https://chunsik.store/user/checkLogin', {
+    method: 'GET',
+  })
     .then(res => {
-      if(res.status = 200){
+      if ((res.status = 200)) {
         return;
-      }else{
+      } else {
         alert('로그인을 하지 않은 유저입니다. 쿠키를 확인해주십시오.');
-        window.location.href='https://chunsik.store:3000/main';
+        window.location.href = 'https://chunsik.store/main';
       }
     })
     .catch(error => {
@@ -42,64 +42,62 @@ function checkLoginStatus(){
     });
 }
 
-function UserProfiles(){
-    fetch('https://chunsik.store:3000/user/userinfo',{
-        method:'GET',
-        headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        credentials:'include'
+function UserProfiles() {
+  fetch('https://chunsik.store/user/userinfo', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    credentials: 'include',
+  })
+    .then(res => {
+      return res.json();
     })
-    .then(res=>{
-        return res.json();
+    .then(json => {
+      const filePath = json.file;
+      const returnNickname = json.nickname;
+      const returnId = json.id;
+      const returnEmail = json.email;
+      const returnInterestGenre = json.interestGenre;
+
+      function showImage(filePath) {
+        const img = document.createElement('img');
+        img.setAttribute('src', filePath);
+        img.setAttribute('alt', '프로필 이미지');
+        profileImage.appendChild(img);
+        return;
+      }
+
+      function setNickname(input) {
+        const p = document.createElement('p');
+        p.innerText = input;
+        nickname.appendChild(p);
+        return;
+      }
+
+      function setEmail(input) {
+        const p = document.createElement('p');
+        p.innerText = input;
+        email.appendChild(p);
+        return;
+      }
+
+      function setUserId(input) {
+        const p = document.createElement('p');
+        p.innerText = input;
+        userId.appendChild(p);
+        return;
+      }
+
+      //TODO interestGenre도 나타나도록 함수 구현하기
+      function setInterestGenres(input) {}
+
+      showImage(filePath);
+      setNickname(returnNickname);
+      setEmail(returnEmail);
+      setUserId(returnId);
     })
-    .then(json=>{
-        const filePath = json.file
-        const returnNickname = json.nickname
-        const returnId = json.id
-        const returnEmail = json.email
-        const returnInterestGenre = json.interestGenre
-
-        function showImage(filePath){
-            const img = document.createElement('img');
-            img.setAttribute('src', filePath);
-            img.setAttribute('alt', '프로필 이미지');
-            profileImage.appendChild(img);
-            return;
-        }
-
-        function setNickname(input){
-            const p = document.createElement('p')
-            p.innerText = input;
-            nickname.appendChild(p);
-            return;
-        }
-
-        function setEmail(input){
-            const p = document.createElement('p')
-            p.innerText = input;
-            email.appendChild(p);
-            return;
-        }
-
-        function setUserId(input){
-            const p = document.createElement('p')
-            p.innerText = input;
-            userId.appendChild(p);
-            return;
-        }
-
-        //TODO interestGenre도 나타나도록 함수 구현하기
-        function setInterestGenres(input){
-            
-        }
-
-        showImage(filePath);
-        setNickname(returnNickname);
-        setEmail(returnEmail)
-        setUserId(returnId);
-    })
-    .catch(err=>{
-        console.log('profile userinfo 연결 중의 에러: ', err);
-    })
+    .catch(err => {
+      console.log('profile userinfo 연결 중의 에러: ', err);
+    });
 }
