347a85f26cfe0d702b2133b8cbf21fb053c52c46
HA0N1 <intor48@gmail.com>
2024-05-02 08:59:26 +0900

    gg

--- public/chat.js
+++ public/chat.js
@@ -13,11 +13,13 @@ document.getElementById('myModal').style.display = 'none';
 let device;
 let rtpCapabilities;
 let producerTransport;
-let consumerTransport;
+let consumerTransports = [];
 let audioProducer;
 let videoProducer;
 let producerId;
-
+let socketId;
+let nickname;
+let roomName;
 // let remoteVideo;
 let params = {
   encoding: [
@@ -46,6 +48,11 @@ const socket = io('/chat', { auth: { token: token } });
 
 let currentRoom = '';
 
+socket.on('connection-success', ({ socketId1, nickname1 }) => {
+  socketId = socketId1;
+  nickname = nickname1;
+});
+
 document.addEventListener('DOMContentLoaded', function () {
   const currentUrl = window.location.href;
   console.log('currentUrl:', currentUrl);
@@ -57,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function createRoomWithModal(event) {
   event.preventDefault(); // 폼 제출 기본 동작 방지
-  const roomName = document.getElementById('roomName').value;
+  roomName = document.getElementById('roomName').value;
   const chatType = document.getElementById('chatType').value;
   const maximumPeople = document.getElementById('maximumPeople').value;
 
@@ -104,17 +111,28 @@ function updateRoomList(rooms) {
     const chatType = await socket.on('chatType', type => {
       return type;
     });
-    joinRoom(room, chatType); // 선택한 방 이름을 인자로 전달하여 joinRoom 함수 호출
+
+    joinRoom(room); // 선택한 방 이름을 인자로 전달하여 joinRoom 함수 호출
   });
 }
 
-function joinRoom(room, chatType) {
+function joinRoom(room) {
   socket.emit('joinRoom', { room });
   $('#chat').html('');
   currentRoom = room;
+  socket.on('chatType', type => {
+    if (type === 'voice') {
+      document.getElementById('container').style.display = 'block';
+      document.getElementById('chatBox').style.display = 'none';
+      getLocalStream();
+    } else {
+      document.getElementById('container').style.display = 'none';
+      document.getElementById('chatBox').style.display = 'block';
+    }
+  });
   socket.emit('requestChatHistory', { room });
-  socket.emit('chatType', { room });
   socket.emit('broadcastScreenSharing', { room });
+  socket.emit('chatType', { room });
 }
 // 채팅 타입별 디스플레이 설정
 socket.on('chatType', type => {
@@ -143,9 +161,9 @@ $(document).ready(function () {
   });
 });
 
-socket.on('connect', () => {
-  console.log('connected', socket.id);
-});
+// socket.on('connect', () => {
+//   console.log('connected', socket.id);
+// });
 
 socket.on('dmHistory', function (dms) {
   $('#chat').html('');
@@ -158,18 +176,21 @@ socket.on('dmHistory', function (dms) {
 //! 1. client : 미디어 수신 정보 서버에 요청
 let audioParams;
 let videoParams = { params };
+let consumingTransports = [];
+let room;
 
 const streamSuccess = async stream => {
   const localVideo = document.getElementById('localVideo');
   localVideo.srcObject = stream;
-  console.log('streamSuccess ~ localVideo:', localVideo.srcObject);
-  console.log('streamSuccess ~ stream:', stream);
 
-  let room = currentRoom;
   audioParams = { track: stream.getAudioTracks()[0], ...audioParams };
   videoParams = { track: stream.getVideoTracks()[0], ...videoParams };
+  room = currentRoom;
 
-  socket.emit('joinVoiceRoom', { room });
+  joinVoiceRoom(room);
+};
+
+async function joinVoiceRoom() {
   await socket.on('getRtpCapabilities', data => {
     // we assign to local variable and will be used when
     // loading the client Device (see createDevice above)
@@ -177,7 +198,8 @@ const streamSuccess = async stream => {
 
     createDevice();
   });
-};
+  socket.emit('joinVoiceRoom', { room, nickname });
+}
 
 const getLocalStream = () => {
   document.getElementById('localVideoOnBtn').style.display = 'none';
@@ -198,6 +220,7 @@ const getLocalStream = () => {
 
 const createDevice = async () => {
   try {
+    console.log('디바이스만드나');
     device = new mediasoupClient.Device();
     await device.load({
       routerRtpCapabilities: rtpCapabilities,
@@ -212,13 +235,12 @@ const createDevice = async () => {
 
 // ! LP = SendTransport 생성을 위한 Transport 생성
 const createSendTransport = async () => {
-  socket.on('createWebRtcTransport1', ({ consumer, params }) => {
-    console.log('createSendTransport transport');
+  // Request to create a new WebRTC Transport
+  socket.off('createWebRtcTransport1').on('createWebRtcTransport1', ({ consumer, params }) => {
     if (params.error) {
       console.log(params.error);
       return;
     }
-
     producerTransport = device.createSendTransport(params);
 
     producerTransport.on('connect', async ({ dtlsParameters }, callback, errback) => {
@@ -226,49 +248,124 @@ const createSendTransport = async () => {
         socket.on('transport-connect', data => {
           const { dtlsParameters } = data;
         });
-        //LP 생성. Send transport
         await socket.emit('transport-connect', { dtlsParameters });
         callback();
-
-        console.log('producerTransport.on ~ producerTransport11111:', producerTransport);
       } catch (error) {
         errback(error);
       }
     });
 
     producerTransport.on('produce', async (parameters, callback, errback) => {
+      console.log('오라ㅏ제발');
+      //! 여기 두번
       try {
-        socket.on('transport-produce', data => {
-          const { id } = data;
-          producerId = id;
-          callback({ id });
-        });
+        // Tell the server to create a Producer with the provided parameters
+        socket.off('transport-produce');
 
+        socket.once('transport-produce', data => {
+          console.log('producerTransport.on ~ data:', data);
+          console.log('프로듀스와라이');
+          const { id, producersExist } = data;
+          console.log('producerTransport.on ~ producersExist:', producersExist);
+          //! 여기 안 이 맞아
+          callback({ id });
+          if (producersExist) {
+            console.log('Other producers exist, initiating room join process or similar action.');
+            getProducers();
+          }
+        });
         await socket.emit('transport-produce', {
           kind: parameters.kind,
           rtpParameters: parameters.rtpParameters,
           appData: parameters.appData,
           dtlsParameters: parameters.dtlsParameters,
         });
-        console.log('producerTransport.on ~ producerTransport2222:', producerTransport);
       } catch (error) {
         errback(error);
       }
     });
-
     connectSendTransport();
   });
   socket.emit('createWebRtcTransport', { consumer: false });
 };
 
+const getProducers = () => {
+  // 서버로부터 'getProducers' 응답을 처리하는 리스너
+  socket.on('getProducers', data => {
+    const { producerList } = data;
+    console.log('getProducers ~ producerList:', producerList);
+
+    // 해당 데이터를 사용하여 필요한 작업 수행
+    producerList.forEach(producer => {
+      const [producerId, producerName, socketId] = producer;
+      console.log(`Producer ID: ${producerId}, Name: ${producerName}, Socket ID: ${socketId}`);
+      signalNewConsumerTransport(producerId, socketId);
+    });
+  });
+
+  // 서버에 'getProducers' 이벤트를 요청
+  socket.emit('getProducers');
+};
+
+const connectRecvTransport = async (consumerTransport, remoteProducerId, serverConsumerTransportId, socketName) => {
+  console.log('connectRecvTransport');
+
+  socket.on('consume', async ({ params }) => {
+    console.log('socket.on ~ params:', params);
+    if (params.error) {
+      console.log('Cannot Consume');
+      return;
+    }
+
+    let consumer = await consumerTransport.consume({
+      id: params.id,
+      producerId: params.producerId,
+      kind: params.kind,
+      rtpParameters: params.rtpParameters,
+    });
+
+    consumerTransports = [
+      ...consumerTransports,
+      {
+        consumerTransport,
+        serverConsumerTransportId: params.id,
+        producerId: remoteProducerId,
+        consumer,
+      },
+    ];
+
+    const { track } = consumer;
+    // remoteVideo.srcObject = new MediaStream([track]);
+
+    const wrapper = document.createElement('div');
+    const newElem = document.createElement('div'); // 비디오 화면
+    const newSpan = document.createElement('span');
+
+    wrapper.setAttribute('id', `td-${remoteProducerId}`);
+
+    newElem.setAttribute('class', 'remoteVideo');
+    newElem.innerHTML =
+      '<video id="' + remoteProducerId + '" autoplay class="video"></video> <p>' + socketName + '</p>';
+
+    wrapper.appendChild(newElem);
+    wrapper.appendChild(newSpan);
+    videoContainer.appendChild(wrapper);
+
+    document.getElementById(remoteProducerId).srcObject = new MediaStream([track]);
+
+    await socket.emit('consumer-resume', { serverConsumerId: params.serverConsumerId });
+  });
+
+  socket.emit('consume', { rtpCapabilities: device.rtpCapabilities, remoteProducerId, serverConsumerTransportId });
+};
 const connectSendTransport = async () => {
-  console.log('connectSendTransport');
-  console.log('test');
+  console.log('connectSendTransport 도착');
+
   audioProducer = await producerTransport.produce(audioParams);
   console.log('connectSendTransport ~ audioProducer:', audioProducer);
 
   videoProducer = await producerTransport.produce(videoParams);
-  console.log('connectSendTransport ~ producerTransport:', videoProducer);
+  console.log('connectSendTransport ~ videoProducer:', videoProducer);
 
   audioProducer.on('trackended', () => {
     console.log('track ended');
@@ -284,84 +381,68 @@ const connectSendTransport = async () => {
   videoProducer.on('transportclose', () => {
     console.log('video transport ended');
   });
-  console.log('connectSendTransport ~ videoProducer:', producerTransport);
-  createRecvTransport();
 };
 
 //! consumer
-const createRecvTransport = async () => {
-  console.log('createRecvTransport');
+const signalNewConsumerTransport = async (remoteProducerId, socketName) => {
+  console.log('createRecvTransport 도착');
+  if (consumingTransports.includes(remoteProducerId)) return;
+  consumingTransports.push(remoteProducerId);
+
   socket.on('createWebRtcTransport2', async ({ consumer, params }) => {
-    console.log('createRecvTransport transport');
+    console.log('socket.on ~ consumer:', consumer);
     if (params.error) {
       console.log(params.error);
       return;
     }
 
-    consumerTransport = device.createRecvTransport(params);
+    console.log(`PARAMS... ${params}`);
+    let consumerTransport;
+    try {
+      consumerTransport = device.createRecvTransport(params);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
 
     consumerTransport.on('connect', async ({ dtlsParameters }, callback, errback) => {
       try {
         socket.on('transport-recv-connect', async data => {
           const { dtlsParameters } = data;
+          console.log('consumerTransport.on ~ dtlsParameters:', dtlsParameters);
         });
-        await socket.emit('transport-recv-connect', { dtlsParameters });
+
+        await socket.emit('transport-recv-connect', {
+          dtlsParameters,
+          serverConsumerTransportId: params.id,
+        });
+
+        // Tell the transport that parameters were transmitted.
         callback();
       } catch (error) {
+        // Tell the transport that something was wrong
         errback(error);
       }
     });
+    connectRecvTransport(consumerTransport, remoteProducerId, params.id, socketName);
   });
-  // 서버에 consumerTransport 생성 요청
   socket.emit('createWebRtcTransport', { consumer: true });
 };
 
-const connectRecvTransport = async () => {
-  console.log('connectRecvTransport');
-  let consumer;
+socket.on('producer-closed', ({ remoteProducerId }) => {
+  // server notification is received when a producer is closed
+  // we need to close the client-side consumer and associated transport
+  const producerToClose = consumerTransports.find(transportData => transportData.producerId === remoteProducerId);
+  producerToClose.consumerTransport.close();
+  producerToClose.consumer.close();
 
-  socket.on('consume', async ({ params }) => {
-    console.log('socket.on ~ params:', params);
-    if (params.error) {
-      console.log('Cannot Consume');
-      return;
-    }
-    const produceId = params.producerId;
-    consumer = await consumerTransport.consume({
-      id: params.id,
-      producerId: params.producerId,
-      kind: params.kind,
-      rtpParameters: params.rtpParameters,
-    });
-    const { track } = consumer;
-    // remoteVideo.srcObject = new MediaStream([track]);
+  // remove the consumer transport from the list
+  consumerTransports = consumerTransports.filter(transportData => transportData.producerId !== remoteProducerId);
 
-    const wrapper = document.createElement('div');
-    const newElem = document.createElement('div'); // 비디오 화면
-    const newSpan = document.createElement('span');
-    // newElem.setAttribute('id', `td-${remoteProducerId}`)
-    wrapper.setAttribute('id', `td-${produceId}`);
+  // remove the video div element
+  videoContainer.removeChild(document.getElementById(`td-${remoteProducerId}`));
+});
 
-    newElem.setAttribute('class', 'remoteVideo');
-    newElem.innerHTML = '<video id="' + produceId + '" autoplay class="video"></video>';
-
-    wrapper.appendChild(newElem);
-    wrapper.appendChild(newSpan);
-    videoContainer.appendChild(wrapper);
-
-    document.getElementById(produceId).srcObject = new MediaStream([track]);
-
-    console.log(
-      'socket.on ~ document.getElementById(produceId).srcObject:',
-      document.getElementById(produceId).srcObject,
-    );
-    await socket.emit('consumer-resume');
-  });
-
-  socket.emit('consume', { rtpCapabilities: device.rtpCapabilities, producerId });
-
-  // await socket.emit('consumer-resume');
-};
 function videoOff() {
   const localVideo = document.getElementById('localVideo');
   const remoteVideo = document.getElementById('remoteVideo'); // Assuming this is the ID of your remote video element
