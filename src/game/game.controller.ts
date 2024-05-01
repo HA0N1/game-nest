efef700cf1d9b0b9ba9e16c2a9e9711f25f6ca74
TriumphOne <woo001232naver.com>
2024-05-01 15:37:29 +0900

    장르별 게임 css 수정

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -84,7 +84,7 @@ export class GameController {
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
   ) {
-    const maxLimit = 50;
+    const maxLimit = 40;
     if (limit > maxLimit) {
       throw new HttpException(`최대 검색 가능 개수는 ${maxLimit}개입니다.`, HttpStatus.BAD_REQUEST);
     }
